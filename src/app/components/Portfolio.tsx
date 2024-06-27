19d1c1c2d5d8d70bdaab574bff7e4978d970bcc6
Indra George <george.indraa@gmail.com>
2024-06-27 19:16:57 +0200

    modify animation

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -24,7 +24,7 @@ const Portfolio = () => {
                         ref={refCapcir}
                         initial={{ opacity: 0}}
                         animate={isInViewCapcir ? { opacity: 1 } : { opacity: 0 }}
-                        transition={{ duration: 0.5, delay: 0.5}}
+                        transition={{ duration: 0.7, delay: 0.5}}
                     >
                         <Project href="https://www.capcir.fr" description="Entreprise tech spécialisée dans les logiciels de santé" technology="Php / React js / Scss" img={capcir} />
                     </motion.div>
@@ -35,13 +35,13 @@ const Portfolio = () => {
                             animate={isInViewPodologue ? { opacity: 1} : { opacity: 0}}
                             transition={{ duration: 0.7, delay: 0.9 }}
                         >
-                            <Project href="https://podologue-7dd17.web.app" description="Cabinet podologue" technology="React js / Bootstrap" img={podologue} />
+                            <Project href="https://podologue-7dd17.web.app" description="Cabinet podologue" technology="React js / Material UI" img={podologue} />
                         </motion.div>
                         <motion.div 
                             ref={refKasa}
                             initial={{ opacity: 0 }}
                             animate={isInViewKasa ? { opacity: 1 } : { opacity: 0}}
-                            transition={{ duration: 0.7, delay: 1.3 }}
+                            transition={{ duration: 0.7, delay: 1.2 }}
                         >
                             <Project href="https://indrageorge.github.io/Kasa" description="Application web de location immobilière" technology="React JS" img={kasa} />
                         </motion.div>
