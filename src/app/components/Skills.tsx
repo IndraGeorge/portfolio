32b63d49f36da93440814402bf7c6f853239a2bf
Indra George <george.indraa@gmail.com>
2024-06-30 17:17:36 +0200

    update module

---
+++ src/app/components/Skills.tsx
@@ -0,0 +1,14 @@
+const Skills = () => {
+    return (
+        <div id="skills" className="flex p-16">
+            <div className="w-[50%]">
+                <h2>Skills</h2>
+            </div>
+            <div className="w-[50%]">
+                listes
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
