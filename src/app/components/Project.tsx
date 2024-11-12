4006827c3fd958fe62f2758285e80db527515122
Indra George <george.indraa@gmail.com>
2024-11-12 17:58:00 +0100

    modify padding

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -15,7 +15,7 @@ const Projects = ({description,technology, img, href}:props) => {
             <Image src={img} alt="img"/>
             <a href={href} target="_blanck" className="description">
                 <h3 className="text-center pl-4 pr-4">{description}</h3>
-                <p className="mt-2">{technology}</p>
+                <p className="mt-2 pl-4 pr-4 text-center">{technology}</p>
                 <Search className="mt-4" size={28} color="#ffffff" />
             </a>
         </div>
